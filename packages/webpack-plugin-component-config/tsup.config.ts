958ec1d486fa25a7b4048f0ca5d5bb1bb0099bf9
chouchouji <1305974212@qq.com>
2025-10-29 14:56:59 +0800

    fix(webpack-plugin-component-config): don't external lodash-es (#2)

---
+++ packages/webpack-plugin-component-config/tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  outDir: 'dist',
+  format: ['esm', 'cjs'],
+  dts: true,
+  clean: true,
+  noExternal: ['lodash-es'],
+});
