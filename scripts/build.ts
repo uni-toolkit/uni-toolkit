33e2c226b589aafab8ed42e61a4902a548b03199
chouchouji <1305974212@qq.com>
2025-11-07 15:14:09 +0800

    chore: add build script

---
+++ scripts/build.ts
@@ -0,0 +1,17 @@
+import { execSync } from "node:child_process";
+
+const packages = process.argv.slice(2);
+
+// build all packages
+if (!packages.length) {
+  execSync(`pnpm --filter './packages/*' build`, { stdio: "inherit" });
+  process.exit(0);
+}
+
+const command = packages.reduce((command, packageName) => {
+  return `${command} --filter './packages/${packageName}'`;
+}, "");
+
+// build input package(s)
+execSync(`pnpm ${command} build`, { stdio: "inherit" });
+process.exit(0);
